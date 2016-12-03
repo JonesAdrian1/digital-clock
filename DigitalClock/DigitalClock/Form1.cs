366476aedcb51ed3783bde3a08b2a74d317529d3
AJ <jones.adrian1@gmail.com>
2016-12-03 00:14:17 -0500

    Added files of project directly from Visual Studio

---
+++ DigitalClock/DigitalClock/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //interval
+            t.Interval = 1000; //in milliseconds
+
+            t.Tick += new EventHandler(this.t_Tick);
+
+            //start timer
+            t.Start(); //this uses t_tick() method
+        }
+
+        //timer eventhandler
+        private void t_Tick(object sender, EventArgs e)
+        {
+
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            //time
+            string time = "";
+
+            //leading zero if hour is less than 10
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+            //update
+            label1.Text = time;
+        }
+    }
+}
